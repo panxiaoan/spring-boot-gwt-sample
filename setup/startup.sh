60a606c9bd67c747a330b84737c9bdd73f3091aa
panxiaoan <xiaoan.pan@qq.com>
2018-04-24 14:41:30 +0800

    增加：以 jar 启动脚本，用来测试 Jenkins

---
+++ setup/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export JAVA_HOME=$JAVA8_HOME
+export CLASSPATH=.:$JAVA_HOME/lib
+export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_OPTS='-server -Xms1200m -Xmx1500m'
+
+java -version
+
+pwd
+
+echo [INFO] Startup APP...
+java -jar target/spring-boot-gwt-sample-0.0.1-SNAPSHOT.jar
+
+
