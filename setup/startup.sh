e655823afce6201700b1327eb7c81fa204379db1
panxiaoan <xiaoan.pan@qq.com>
2018-04-24 14:59:38 +0800

    简单修改~

--- setup/startup.sh
+++ setup/startup.sh
@@ -5,11 +5,13 @@ export CLASSPATH=.:$JAVA_HOME/lib
 export PATH=$JAVA_HOME/bin:$PATH
 export JAVA_OPTS='-server -Xms1200m -Xmx1500m'
 
-java -version
-
+echo [INFO] Current Project Path
 pwd
 
+echo [INFO] Java version
+java -version
+
 echo [INFO] Startup APP...
-nohup java -jar target/spring-boot-gwt-sample-0.0.1-SNAPSHOT.jar  > /dev/null 2&1 &
+nohup java -jar target/spring-boot-gwt-sample-0.0.1-SNAPSHOT.jar &
 
 
