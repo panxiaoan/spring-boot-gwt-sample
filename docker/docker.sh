da323418b763b4cdbfa1c749db3d055bac3fd2ba
panxiaoan <xiaoan.pan@qq.com>
2018-05-16 16:12:42 +0800

    增加 Docker 配置文件，用于 Jenkins 自动构建、发布

--- docker/docker.sh
+++ docker/docker.sh
@@ -6,12 +6,13 @@ BUILD_ID=$1
 COMMIT_ID=$2
 CONTAINER_NAME=spring-boot-gwt-sample
 IMAGES_NAME=java/$CONTAINER_NAME
-echo "build_id:"$1" commit_id:"$2" buildtime:"`date "+%Y-%m-%d %H:%M:%S"` >> docker_build.log
 
+echo "build_id:"$1" commit_id:"$2" buildtime:"`date "+%Y-%m-%d %H:%M:%S"` >> docker.log
+
+# 查找待部署的 SpringBoot 应用
 FILENAME=$(find -name spring-boot-gwt-sample-**.jar)
 JARNAME=${FILENAME##*/}
-chmod 777 $JARNAME
-
+chmod  777 $JARNAME
 if [ -z "$JARNAME" ]
 then
     echo "not find :"$JARNAME
@@ -19,10 +20,12 @@ then
 else
     echo "find app:"$JARNAME
 fi
- 
+
+# 停止并删除原有容器 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
+# 删除原有容器镜像
 IMAGE_ID=$(docker images | grep "$IMAGES_NAME" | awk '{print $3}')
 echo "iam:"$IMAGE_ID
 if [ -z "$IMAGE_ID" ]
@@ -33,7 +36,8 @@ else
     docker rmi -f $IMAGE_ID
 fi
 
-docker build --build-arg app=$JARNAME . -t $IMAGES_NAME:$BUILD_ID
-rm $JARNAME
-
-docker run -itd -p 8080:8080 --name $CONTAINER_NAME $IMAGES_NAME:$BUILD_ID --restart=always
+# 构建容器
+docker build --build-arg app=$JARNAME .  -t  $IMAGES_NAME:$BUILD_ID
+ 
+# 运行容器
+docker run -d -p 8080:8080 --name $CONTAINER_NAME  $IMAGES_NAME:$BUILD_ID
