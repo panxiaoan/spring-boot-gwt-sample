516c05bbd0f06b9e15651cc7b761f35ba6b732cd
panxiaoan <xiaoan.pan@qq.com>
2018-06-20 11:33:49 +0800

    增加自动更新脚本

---
+++ kubernetes/kube-rollupdate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /opt/spring-boot-gwt-sample-docker
+
+BUILD_ID=$1
+
+echo "rollup deployment……"
+kubectl set image deployment/springbootgwtsample springbootgwtsample=panxiaoan/spring-boot-gwt-sample:$BUILD_ID --namespace=default
